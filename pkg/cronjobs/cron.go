b81f9019110e5619dafde8eb4ac51a48af24b295
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2024-02-16 21:52:33 +0000

    changed cron job

--- pkg/cronjobs/cron.go
+++ pkg/cronjobs/cron.go
@@ -10,7 +10,7 @@ import (
 func Start() {
 	c := cron.New()
 	c.AddFunc("0 * * * *", search.RunEngine) // Run every hour
-	c.AddFunc("0 15 * * *", search.RunIndex) // Run every hour at 15 minutes past
+	c.AddFunc("15 * * * *", search.RunIndex) // Run every hour at 15 minutes past
 	c.Start()
 	cronCount := len(c.Entries())
 	fmt.Printf("setup %d cron jobs \n", cronCount)
