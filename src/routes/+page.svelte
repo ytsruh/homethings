3dfc8bf95f12322f0b5301a8b46c60cbfa64ca0f
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2024-10-29 17:43:04 +0000

    feat: setup Shadcn library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+ import { Button } from "$lib/components/ui/button/index.js";
+ import ChevronRight from "lucide-svelte/icons/chevron-right";
+</script>
+ 
 <h1>Welcome to SvelteKit</h1>
+<div class="flex flex-row gap-5 py-5">
+<Button variant="outline" size="icon">
+ <ChevronRight class="size-4" />
+</Button>
+ 
+<Button>Click me</Button>
+</div>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,13 @@
-// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://svelte.dev/docs/kit/integrations
-// 	// for more information about preprocessors
-// 	preprocess: vitePreprocess(),
-
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
-
 import adapter from "@sveltejs/adapter-cloudflare";
 
 export default {
   preprocess: vitePreprocess(),
   kit: {
+    alias: {
+      "@/*": "./src/*",
+      "@lib/*": "./src/lib/*",
+    },
     adapter: adapter({
       // See below for an explanation of these options
       routes: {
