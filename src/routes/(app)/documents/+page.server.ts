d93c183599a092cd568a4bfda5549ffb8ea08014
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2024-11-03 19:03:28 +0000

    feat: added basic data table

---
+++ src/routes/(app)/documents/+page.server.ts
@@ -0,0 +1,28 @@
+type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+  // ...
+];
+
+export async function load() {
+  return {
+    payments: data,
+  };
+}
---
+++ src/routes/(app)/documents/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import PageHeader from "@/lib/components/PageHeader.svelte";
+  import DocumentsTable from "./DocumentsTable.svelte";
+  import { columns } from "./columns";
+
+  let { data } = $props();
+</script>
+
+<svelte:head>
+  <title>Documents | Homethings</title>
+  <meta name="description" content="Document storage shared across your account" />
+</svelte:head>
+
+<PageHeader title="Documents" subtitle="Document storage shared across your account" />
+<DocumentsTable data={data.payments} {columns} />
