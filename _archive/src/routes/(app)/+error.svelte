21418f6df3fde6dcfdbe40adbbdcbae06c4e908e
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2025-04-05 11:34:46 +0100

    feat: new migrations & removed old svelte project

--- _archive/src/routes/(app)/+error.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { page } from "$app/stores";
-</script>
-
-<div class="flex flex-col items-center justify-center">
-  <h1>{$page.status}</h1>
-  <h2>{$page.error?.message || "Something went wrong"}</h2>
-</div>
