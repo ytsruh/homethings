f5f6b82f420551c6bf1f850f69849248f2ff5e73
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2025-09-27 16:50:30 +0100

    feat: removed tasks toggle from profile

--- client/app/routes/app/profile.tsx
+++ client/app/routes/app/profile.tsx
@@ -8,9 +8,13 @@ import { pb } from "~/lib/utils";
 import { Switch } from "~/components/ui/switch";
 import { toast } from "~/components/Toaster";
 import { ZodError } from "zod";
+import { useId } from "react";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "Profile" }, { name: "description", content: "Welcome to Homethings" }];
+  return [
+    { title: "Profile" },
+    { name: "description", content: "Welcome to Homethings" },
+  ];
 }
 
 export async function clientLoader({}: Route.ClientLoaderArgs) {
@@ -34,7 +38,6 @@ export async function clientAction({ request }: Route.ClientActionArgs) {
     const name = formData.get("name");
     const showChat = formData.get("show_chat");
     const showNotes = formData.get("show_notes");
-    const showTasks = formData.get("show_tasks");
     const user = pb.authStore.record;
     if (!user) {
       pb.authStore.clear();
@@ -44,7 +47,6 @@ export async function clientAction({ request }: Route.ClientActionArgs) {
       name,
       showChat: showChat === "on",
       showNotes: showNotes === "on",
-      showTasks: showTasks === "on",
     });
     toast({
       description: "Profile updated",
@@ -66,31 +68,44 @@ export async function clientAction({ request }: Route.ClientActionArgs) {
 
 export default function Profile({ loaderData }: Route.ComponentProps) {
   const fetcher = useFetcher();
+  const id = useId();
   const { user } = loaderData;
   return (
     <>
       <PageHeader title="Profile" subtitle="Manage your profile" />
-      <fetcher.Form method="post" className="flex flex-col w-full max-w-xl items-center gap-4 py-2">
+      <fetcher.Form
+        method="post"
+        className="flex flex-col w-full max-w-xl items-center gap-4 py-2"
+      >
         <div className="grid w-full items-center gap-1.5">
           <Label htmlFor="email">Email</Label>
           <Input defaultValue={user?.email || ""} disabled />
         </div>
         <div className="grid w-full items-center gap-1.5">
           <Label htmlFor="name">Name</Label>
-          <Input type="text" name="name" placeholder="Name" defaultValue={user?.name || ""} />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Name"
+            defaultValue={user?.name || ""}
+          />
         </div>
         <div className="grid w-full grid-cols-1 lg:grid-cols-2 gap-4">
           <div className="w-full flex items-center justify-between">
             <Label htmlFor="show_chat">Show Chat</Label>
-            <Switch defaultChecked={user?.showChat || false} id="show_chat" name="show_chat" />
+            <Switch
+              defaultChecked={user?.showChat || false}
+              id={id}
+              name="show_chat"
+            />
           </div>
           <div className="w-full flex items-center justify-between">
             <Label htmlFor="show_notes">Show Notes</Label>
-            <Switch defaultChecked={user?.showNotes || false} id="show_notes" name="show_notes" />
-          </div>
-          <div className="w-full flex items-center justify-between">
-            <Label htmlFor="show_tasks">Show Tasks</Label>
-            <Switch defaultChecked={user?.showTasks || false} id="show_tasks" name="show_tasks" />
+            <Switch
+              defaultChecked={user?.showNotes || false}
+              id={id}
+              name="show_notes"
+            />
           </div>
         </div>
         <div className="flex justify-end w-full gap-1.5">
