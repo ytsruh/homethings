1bdb2439079ae52cf2f80a94c98515e919261525
Chris Hurst <ytsruh@gmail.com>
2026-02-28 17:35:18 +0000

    feat: reset migrations

--- server/drizzle/0000_concerned_bullseye.sql
+++ server/drizzle/0000_concerned_bullseye.sql
@@ -7,17 +7,6 @@ CREATE TABLE `feedback` (
 	FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE TABLE `note_attachments` (
-	`id` text PRIMARY KEY NOT NULL,
-	`note_id` text NOT NULL,
-	`file_key` text NOT NULL,
-	`file_name` text NOT NULL,
-	`file_type` text NOT NULL,
-	`file_size` integer NOT NULL,
-	`created_at` integer NOT NULL,
-	FOREIGN KEY (`note_id`) REFERENCES `notes`(`id`) ON UPDATE no action ON DELETE cascade
-);
---> statement-breakpoint
 CREATE TABLE `notes` (
 	`id` text PRIMARY KEY NOT NULL,
 	`title` text NOT NULL,
