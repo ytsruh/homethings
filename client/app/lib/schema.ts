e6ae54cc11758c9e8413eef2e2e817737cad7bbd
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2026-03-01 20:04:39 +0000

    chore: upgrade zod to v4

--- client/app/lib/schema.ts
+++ client/app/lib/schema.ts
@@ -1,7 +1,7 @@
-import { z } from "zod";
+import { z } from "zod/v4";
 
 export const loginForm = z.object({
-	email: z.string().email("Please enter a valid email address"),
+	email: z.email("Please enter a valid email address"),
 	password: z
 		.string()
 		.min(6, "Please enter a password of at least 8 characters"),
