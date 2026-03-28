4ea47dbb6006879ea11976865b14d85076ccee77
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2026-03-28 21:59:09 +0000

    fix: read correct env in prod

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -8,8 +8,8 @@ const isRemote =
 
 const turso = createClient({
 	url: "file:local.db",
-	syncUrl: isRemote ? process.env.DATABASE_URL : undefined,
-	authToken: isRemote ? process.env.DATABASE_AUTH_TOKEN : undefined,
+	syncUrl: isRemote ? process.env.TURSO_DATABASE_URL : undefined,
+	authToken: isRemote ? process.env.TURSO_AUTH_TOKEN : undefined,
 	syncInterval: isRemote ? 60 : undefined,
 });
 
